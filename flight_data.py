0d7bd6ef16241ce1caa1a7a7749d05d2894b720d
Dibyanshu Pandey <dibyansh01iitk@gmail.com>
2023-07-14 02:21:48 +0530

    Update flight_data.py

--- flight_data.py
+++ flight_data.py
@@ -1,7 +1,4 @@
 import requests
-
-
-
 class FlightData:
     #This class is responsible for structuring the flight data.
     def __init__(self, price, origin_city,origin_airport, destination_city, destination_airport, out_date, return_date):
